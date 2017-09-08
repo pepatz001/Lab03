1bc5bc6b8265b7da765de7cb82a90cc909d6b47e
DESKTOP-BBH54MA\hp <sirta.pepat@g.swu.ac.th>
2017-09-08 14:09:16 +0700

    searchBeers

--- js/api.js
+++
@@ -1,3 +0,0 @@
-const getGithubProfile = () =>
-  fetch('https://api.github.com/users/pepatz001')
-export default getGithubProfile
--- js/main.js
+++ js/main.js
@@ -1,5 +1,82 @@
-import api from './api'
-api()
-  .then(response => response.json())
-  .catch(e => console.error('Something went wrong', e))
-  .then(response => console.log('response', response))
+var getBeersApi = function getBeersApi(){
+  return fetch('https://api.punkapi.com/v2/beers').then(function (response) {
+    return response.json();
+  }).catch(function(e) {
+    return console.error('Fail');
+  }).then(function(response) {
+    var beers = response;
+    var length = beers.length;
+    var tagHtml = "<table>"+
+    "<tr>"+
+      "<th>Name</th>"+
+      "<th>Tagline</th>"+
+      "<th>First Brewed</th>"+
+      "<th>Description</th>"+
+      "<th>Image</th>"+
+    "</tr>";
+    for(var i = 0; i < length; i++){
+      tagHtml += "<tr>"+
+          "<td>" + beers[i].name + "</td>"+
+          "<td>" + beers[i].tagline + "</td>"+
+          "<td>" + beers[i].first_brewed + "</td>"+
+          "<td>" + beers[i].description + "</td>"+
+          "<td><image src=" + beers[i].image_url + " height='200px;'/></td>"+
+        "</tr>";
+    }
+    tagHtml += "</html>";
+    console.log('Tag HTML',response);
+    document.getElementById('beer_table').innerHTML = tagHtml;
+  })
+};
+getBeersApi();
+
+var searchBeers = function searchBeers(){
+  return fetch('https://api.punkapi.com/v2/beers').then(function (response) {
+    return response.json();
+  }).catch(function(e) {
+    return console.error('Fail');
+  }).then(function(response) {
+    var beers = response;
+    var length = beers.length;
+    var searchBeers = document.getElementById('search').value;
+    var tagHtml = "<table>"+
+    "<tr>"+
+      "<th>Name</th>"+
+      "<th>Tagline</th>"+
+      "<th>First Brewed</th>"+
+      "<th>Description</th>"+
+      "<th>Image</th>"+
+    "</tr>";
+
+    var re = new RegExp(searchBeers.toLowerCase(), 'gi');
+    for(var i = 0; i < length; i++){
+      var str = "" + beers[i].name + " " + beers[i].tagline + " " + beers[i].first_brewed + " " + beers[i].description;
+      
+      var res = str.match(re);
+      //console.log('res :',res);
+      if(res){
+        tagHtml += "<tr>"+
+          "<td>" + beers[i].name + "</td>"+
+          "<td>" + beers[i].tagline + "</td>"+
+          "<td>" + beers[i].first_brewed + "</td>"+
+          "<td>" + beers[i].description + "</td>"+
+          "<td><image src=" + beers[i].image_url + " height='200px;'/></td>"+
+        "</tr>";
+      }
+      
+    }
+    tagHtml += "</html>";
+    //console.log('Tag HTML',response);
+    tagHtml = tagHtml.replace(re, "<span style='background-color: yellow;'>" + searchBeers + "</span>");
+    document.getElementById('beer_table').innerHTML = tagHtml;
+  })
+};
+window.onload = function(){ 
+  document.getElementById("searchBtn").onclick = function() {
+    if(document.getElementById('search').value != ""){
+      searchBeers()
+    } else {
+      getBeersApi();
+    }
+  };
+};
